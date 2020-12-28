490705397f37a2cd61fdccc0e30abce90705605e
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2020-12-28 13:11:34 -0600

    removed dev files

--- src/pages/typography.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactTypographyPage from '../../../gatsby-theme-jim-theme-ui/src/pages/ReactTypographyPage'
-
-const typography = () => (
-  <ReactTypographyPage/>
-)
-
-export default typography
