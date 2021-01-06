fe48fa846bff99d6bc892833f55d6829b3144ca5
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2021-01-05 18:07:36 -0600

    Updates

---
+++ component---src-pages-404-js-a4f5a134a82c4313a488.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("mXGw"),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a4f5a134a82c4313a488.js.map
